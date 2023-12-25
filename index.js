89d131e4650f1200049c764d2908dfbeb2f45aac
Enthusiastic <belaybirhanu407@gmail.com>
2023-12-25 22:53:34 +0300

    one time commit

--- index.js
+++ index.js
@@ -1,24 +1,54 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const bodyParser = require("body-parser");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+const urlDatabase = {};
+let shortUrlCounter = 1;
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post("/api/shorturl", (req, res) => {
+  const originalUrl = req.body.url;
+
+  // Validate URL format
+  const urlRegex = /^(ftp|http|https):\/\/[^ "]+$/;
+  if (!urlRegex.test(originalUrl)) {
+    return res.json({ error: "invalid url" });
+  }
+
+  // Store in-memory mapping
+  const shortUrl = shortUrlCounter++;
+  urlDatabase[shortUrl] = originalUrl;
+
+  // Respond with JSON
+  res.json({ original_url: originalUrl, short_url: shortUrl });
 });
 
-app.listen(port, function() {
+app.get("/api/shorturl/:short_url", (req, res) => {
+  const shortUrl = req.params.short_url;
+  const originalUrl = urlDatabase[shortUrl];
+
+  if (originalUrl) {
+    res.redirect(originalUrl);
+  } else {
+    res.json({ error: "short url not found" });
+  }
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
